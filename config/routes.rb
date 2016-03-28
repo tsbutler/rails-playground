b10486bf438a42e9baeaf4934a031f5b99050b0c
Thorin Butler <thorin.butler@gmail.com>
2016-03-28 14:11:30 -0500

    added administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :users
+resources :movies
+
+    root to: "users#index"
+  end
+
   devise_for :users
   get "movies/list" => "movies#list"
   get "movies/:id" => "movies#show"
